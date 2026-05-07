43865a4af4f07aa16ca26db020bdd0c162427eb8
José Antonio <joscasgar1@alum.us.es>
2026-05-07 19:45:19 +0200

    arreglado problema de dependencias

--- src/main/java/aiss/YouTubeMiner/YouTubeMinerApplication.java
+++ src/main/java/aiss/YouTubeMiner/YouTubeMinerApplication.java
@@ -2,6 +2,9 @@ package aiss.YouTubeMiner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 @SpringBootApplication
 public class YouTubeMinerApplication {
@@ -10,4 +13,6 @@ public class YouTubeMinerApplication {
 		SpringApplication.run(YouTubeMinerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) { return builder.build(); }
 }
