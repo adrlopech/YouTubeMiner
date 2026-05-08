4f4bac614fa6160258e50b3d6fd442d9d02b243c
Paula-Arias11 <pauarijim@alum.us.es>
2026-05-08 20:53:27 +0200

    Autenticación ApiKey

--- src/main/java/aiss/YouTubeMiner/service/VideoMinerService.java
+++ src/main/java/aiss/YouTubeMiner/service/VideoMinerService.java
@@ -3,8 +3,11 @@ package aiss.YouTubeMiner.service;
 import aiss.YouTubeMiner.model.videominer.Channel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders; // IMPORT CORRECTO
+import org.springframework.http.MediaType;
 
 @Service
 public class VideoMinerService {
@@ -16,8 +19,16 @@ public class VideoMinerService {
     private String videoMinerUrl;
 
     public Channel postChannel(Channel channel) {
-        String url = videoMinerUrl + "/videominer/channels";
-        // Enviamos el canal transformado a VideoMiner
-        return restTemplate.postForObject(url, channel, Channel.class);
+        String url = "http://localhost:8080/videominer/api/channels";
+
+        // 1. Creamos las cabeceras y metemos la API Key
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-API-KEY", "trabajoAiss123");
+
+        // 2. Metemos el canal y las cabeceras en un "paquete" llamado HttpEntity
+        HttpEntity<Channel> request = new HttpEntity<>(channel, headers);
+
+        // 3. Hacemos el POST enviando el paquete completo (request)
+        return restTemplate.postForObject(url, request, Channel.class);
     }
 }
